892a8b3c5e155dea386a297e99f3989c776e62c7
Ben Lopatin <ben@benlopatin.com>
2023-07-07 11:07:08 -0400

    Remove ordereddict
    This is also not required with Python 3

--- setup.py
+++ setup.py
@@ -19,11 +19,6 @@ def get_install_requires():
     except ImportError:
         install_requires.append('importlib')
 
-    try:
-        from collections import OrderedDict
-    except ImportError:
-        install_requires.append('ordereddict')
-
     return install_requires
 
 setup(
